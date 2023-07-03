3505397b1196bd781856034ba0b71bf7721bef28
VASANTHKUMAR B <53816549+vasanthkumarb29@users.noreply.github.com>
2023-07-03 15:09:49 +0530

    Add files via upload

---
+++ Unit1/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const port = 3001;
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', (req, res) => {
+    var name = req.body.name;
+    var height = parseFloat(req.body.height);
+    var weight = parseFloat(req.body.weight);
+    var BMI = weight / (height ** 2);
+    res.send(`Hello ${name}, your BMI is ${BMI}`);
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
